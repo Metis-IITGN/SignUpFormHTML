03e878305cdcbc54c599c8cc0f5c5a460f8feab8
Greeshma97 <cr.greeshma@iitgn.ac.in>
2016-12-12 00:13:41 +0530

    verify with signup.php
    signup.php by greeshma

---
+++ signup.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>VERIFICATION PAGE</h1>
+Name: <?php echo $_POST["name"]; ?><br>
+Email:<?php echo $_POST["email"]; ?><br>
+Roll No:<?php echo $_POST["roll"]?><br>
+Course:<?php echo $_POST["course"]?><br>
+Password:<?php echo $_POST["password"]?><br>
+</body>
+</html>
+
