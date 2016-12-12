5694debcb7121c49c7b7cd08e0a32fb444eb8a12
Greeshma97 <cr.greeshma@iitgn.ac.in>
2016-12-13 00:00:02 +0530

    signup.php by greeshma
    Added filter features and database creation

--- signup.php
+++ signup.php
@@ -1,11 +1,23 @@
-<html>
-<body>
-<h1>VERIFICATION PAGE</h1>
-Name: <?php echo $_POST["name"]; ?><br>
-Email:<?php echo $_POST["email"]; ?><br>
-Roll No:<?php echo $_POST["roll"]?><br>
-Course:<?php echo $_POST["course"]?><br>
-Password:<?php echo $_POST["password"]?><br>
-</body>
-</html>
+<?php
+$name=$_POST["name"];
+$email=$_POST["email"];
+$roll=$_POST["roll"];
+$course=$_POST["course"];
+$password=$_POST["password"];
+$datastore=mysqli_connect();
+
+if  ($email==NULL or $password==NULL)
+{  echo "Mandatory Fields left empty!!";}
+
+else
+{  mysqli_query($datastore,"INSERT INTO registered_users(Name,Email,RollNo,Course,Password) VALUES ($name,$email,$roll,$course,$password");
+   echo "<h1>VERIFICATION PAGE</h1>";
+   echo "Name:".$name."<br>";
+   echo "Email:".$email."<br>";
+   echo "Roll No:".$roll."<br>";
+   echo "Course:".$course."<br>";
+   echo "Password:".$password."<br>";}
+
+mysqli_close($datastore);
+?>
 
