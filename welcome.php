d74a7705a3129d4316c7aa6dae7272fe2d14e96e
phantomrush <rushali.saxena@iitgn.ac.in>
2016-12-13 23:01:26 +0530

    trash file to redirect to a page once signedup
    can be deleted

---
+++ welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>
+            Welcome to TeraShare
+        </title>
+    
+    </head>
+    <body>
+        You have successfully signed up!!!
+    
+    </body>
+</html>
