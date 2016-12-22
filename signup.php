47ea9f7d61498ed4b9aff004cce47d77d3811192
Greeshma97 <cr.greeshma@iitgn.ac.in>
2016-12-22 23:28:54 +0530

    Updated Signup.php
    html also added and updated for storage

--- signup.php
+++ signup.php
@@ -1,8 +1,48 @@
-session_start();
-
 <html>
 <body>
+<form method="post" action="signup.php">
+   <h1> Sign Up</h1>
+   <fieldset>
+   Email Address*:
+   <input type="text" name="email" placeholder="peter.parker" >@iitgn.ac.in
+   <br>
+   Name:
+   <input type="text" name="name" placeholder="Peter Parker">
+   <br>
+   Roll No:
+   <input type="text" name="roll" placeholder="16110038">
+   <br>
+<!--Course: USING RADIO BUTTON
+   <input type="radio" name="course" value="btech" checked> BTech <br>
+   <input type="radio" name="course" value="mtech "> Mtech<br>
+   <input type="radio" name="course" value="msc"> MSc<br>
+   <input type="radio" name="course" value="ma"> MA<br>
+   <input type="radio" name="course" value="phd"> PHD<br>
+   <input type="radio" name="course" value="alumni"> Alumni<br>
+HERE WE USE DROPDOWN MENU-->
+   Course:<select name="course">
+	<option value="btech">Btech</option>
+	<option value="mtech">Mtech</option>
+	<option value="msc">MSc</option>
+	<option value="ma">MA</option>
+	<option value="phd">PHD</option>
+	<option value="alumni">Alumni</option>
+   </select><br>
+   Password*:
+   <input type="password" name="password">
+   <br>
+   Verify Password:
+   <input type="password" name="password2">
+   <br>
+   *Mandatory Fields	
+   </fieldset>
+   <br><input type="submit" name="submit_button" value="Submit"><br> 
+</form>
+ 
    <?php
+   session_start();
+if (isset($_POST["submit_button"]))
+{
    $name=$_POST["name"];
    $email=$_POST["email"];
    $roll=$_POST["roll"];
@@ -33,7 +73,7 @@ session_start();
 
    else
    {  mysqli_query($datastore,"INSERT INTO registered_users(Name,Email,RollNo,Course,Password) VALUES ($name,$email,$roll,$course,$password)");
-       echo "<h1>VERIFICATION PAGE</h1>";
+       echo "<h2>VERIFICATION PAGE</h2>";
        echo "Name:".$name."<br>";
        echo "Email:".$email."<br>";
        echo "Roll No:".$roll."<br>";
@@ -43,9 +83,7 @@ session_start();
        echo "Get through to the<a href='welcomepage.php'> WORLD OF SHARING</a>";
      }
    mysqli_close($datastore);
+}
    ?>
 </body>
 </html>
-
-
-
