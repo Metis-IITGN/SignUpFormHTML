86b7301924c0a2bc0ad4e72771db6d13c59a03c2
phantomrush <rushali.saxena@iitgn.ac.in>
2016-12-31 17:01:53 +0530

    v1 logout page

--- logout.php
+++ logout.php
@@ -1,5 +1,32 @@
+<?php
+    session_start();
+    session_destroy();
+?>
+<!DOCTYPE>
 <html>
-<body>
-You are Successfully Signed Out
-</body>
+    <head>
+        <title>Sign Up for TeraShare</title>
+    </head>
+    <link type="text/css" rel="stylesheet" href="css/css_for_signup.css">
+    <body>
+        <ul id="header">
+              <li><a class="alert" href="#SignUp">Sign Up</a></li>
+              <li><a href="#ContactUs">Contact Us</a></li>
+              <li><a href="#FAQ">FAQs</a></li>
+              <li><a href="#Login">Login</a></li>
+            </ul>
+        
+        <div class="navigationbar">
+      <h1>Tera share</h1>
+	  <h4>Welcomes you</h4>
+    </div><br><br>
+        
+        
+        <div color="white"><b>You have logged out successfully!</b></div>
+        <footer>
+<h3>Page Developed and Maintained by <b>Team TeraShare</b></h3>
+</footer>
+    
+    </body>
+
 </html>
