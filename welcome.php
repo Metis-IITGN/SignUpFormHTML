83250183f52334f273ff73e1e5728627b183ddfd
phantomrush <rushali.saxena@iitgn.ac.in>
2016-12-31 16:58:48 +0530

    sign in logout feature implemented properly
    redirected to sign up page though. Have to redirect to login page.

--- welcome.php
+++ welcome.php
@@ -1,3 +1,17 @@
+<?php
+session_start();
+
+function error_found(){
+  header("Location: signuprush.php");
+}
+set_error_handler('error_found');
+if ($_SESSION["loggedin"] === "no")
+{
+    echo "You are not logged in";
+}
+else
+{
+    echo "
 <!DOCTYPE>
 <html>
     <head>
@@ -7,7 +21,11 @@
     
     </head>
     <body>
-        You have successfully signed up!!!
+        You have successfully signed up!!!<br>
+        <a href=\"logout.php\">Logout</a>
     
     </body>
 </html>
+";
+}
+?>
