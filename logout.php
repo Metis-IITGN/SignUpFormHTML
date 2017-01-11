a88906d35ef1d510850e758535827c38bc8d979f
phantomrush <rushali.saxena@iitgn.ac.in>
2017-01-11 17:34:24 +0530

    link to sign in again after logout added

--- logout.php
+++ logout.php
@@ -10,7 +10,7 @@
     <link type="text/css" rel="stylesheet" href="css/css_for_signup.css">
     <body>
         <ul id="header">
-              <li><a class="alert" href="#SignUp">Sign Up</a></li>
+              <li><a href="#SignUp">Sign Up</a></li>
               <li><a href="#ContactUs">Contact Us</a></li>
               <li><a href="#FAQ">FAQs</a></li>
               <li><a href="#Login">Login</a></li>
@@ -22,7 +22,9 @@
     </div><br><br>
         
         
-        <div color="white"><b>You have logged out successfully!</b></div>
+        <div color="white"><b>You have logged out successfully!</b><br>
+			<a href="login.php">Login to use TersShare</a><br>
+		</div>
         <footer>
 <h3>Page Developed and Maintained by <b>Team TeraShare</b></h3>
 </footer>
