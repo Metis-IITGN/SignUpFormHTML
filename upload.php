a48b458db9b497bb64eb8659254d460c2b7697cb
phantomrush <rushali.saxena@iitgn.ac.in>
2017-01-14 20:14:54 +0530

    shows error messages if upload doesn't work
    Upload file size has to be adjusted. Also, page front end must be worked on.

--- upload.php
+++ upload.php
@@ -1,4 +1,24 @@
 <?php
-$target_dir="uploads/";
-$target_file=$target_dir.basename($_FILES["file2upload"]["name"]);
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
 ?>
