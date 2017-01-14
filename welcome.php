9d418d0c4ea1bfc988acb50060d9a216321ffb7f
phantomrush <rushali.saxena@iitgn.ac.in>
2017-01-14 20:12:59 +0530

    upload file optioin added. Has to be modified.

--- welcome.php
+++ welcome.php
@@ -23,6 +23,12 @@ else
     <body>
         You have successfully signed up!!!<br>
         <a href=\"logout.php\">Logout</a>
+		
+		<form action=\"upload.php\" method=\"post\" enctype=\"multipart/form-data\">
+    Select image to upload:
+    <input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\">
+    <input type=\"submit\" value=\"Upload Image\" name=\"submit\">
+</form>
     
     </body>
 </html>
