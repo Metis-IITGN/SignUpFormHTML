eba932de46839e54f77300354d5aba2fbe0d4187
phantomrush <rushali.saxena@iitgn.ac.in>
2017-01-21 15:21:04 +0530

    files added to respective folder
    also records which user uploaded the file

--- upload.php
+++ upload.php
@@ -1,9 +1,28 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+session_start();
+
+
+$db_username = "root";
+    $db_password = "";
+    $db = "names";   $db=mysqli_connect("localhost",$db_username,$db_password,$db) or die("Failed to connect to database".mysql_error());
+
+function error_found(){
+  header("Location: signuprush.php");
+}
+set_error_handler('error_found');
+if ($_SESSION["loggedin"] === "no")
+{
+    echo "You are not logged in";
+}
+else
+{
+	
+		$target_fol = (string)$_POST["course_initials"];	
+$target_dir = "uploads/".$target_fol."/";
+	$target_fol = $target_fol + '';
+$target_file=$target_dir.$_POST["course_initials"]."_".$_POST["course_code"]."_".basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
+$FileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
@@ -15,10 +34,16 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		
+		$q = "INSERT INTO files(folder,file_name,uploaded_by) VALUES('".($_POST["course_initials"])."','".($_POST["course_initials"]."_".$_POST["course_code"]."_".basename($_FILES["fileToUpload"]["name"]))."','".$_SESSION["email"]."');";
+		
+		mysqli_query($db,$q) or die(mysql_error());
+		
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
+}
 }
 
 ?>
